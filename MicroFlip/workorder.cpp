31198f23c267c4692ba0113aaba8e7e85bac78fd
Muesli_Reep <project_bravo@hotmail.com>
2018-06-30 00:04:00 +0200

    Turned off workorder price increase

--- MicroFlip/workorder.cpp
+++ MicroFlip/workorder.cpp
@@ -121,7 +121,7 @@ void WorkOrder::createSellOrder(double amount) {
   //sellPrice = currentTicker.getLast() + 0.5;
 
   // Match current sell order
-  sellPrice = currentTicker.getBuy() * 1.19;
+  sellPrice = currentTicker.getBuy();// * 1.19;
 
   // Check balance
   // TODO
